e466941d95193f6b6c19efe26b7921122923278a
Kamran Ali <kamran.ali@xerobug.com>
2016-01-31 21:37:41 +0500

    camera changes

--- js/pages/sale-photo.js
+++ js/pages/sale-photo.js
@@ -2,13 +2,13 @@
 // this function will capture customer photo
 function capturePhotoWithData() {
 	// Take picture using device camera and retrieve image as base64-encoded string
-	navigator.camera.getPicture(onPhotoDataSuccess, onFail, { quality: 25,targetWidth: 415,targetHeight: 515,allowEdit : false, });
+	navigator.camera.getPicture(onPhotoDataSuccess, onFail, {sourceType:1, quality: 25,targetWidth: 415,targetHeight: 515,allowEdit : false, });
 }
 
 // this function will capture customer photo
 function capturePhotoWithData2() {
 	// Take picture using device camera and retrieve image as base64-encoded string
-	navigator.camera.getPicture(onPhotoDataSuccess2, onFail, { quality: 25,targetWidth: 415,targetHeight: 515 });
+	navigator.camera.getPicture(onPhotoDataSuccess2, onFail, {sourceType:1, quality: 25,targetWidth: 415,targetHeight: 515 });
 }
 // Called if something bad happens.
 // 
