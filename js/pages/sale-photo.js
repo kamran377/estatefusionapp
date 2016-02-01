cdfdd47205086eb8f4836ad2f17a45374ecbed1b
Kamran Ali <kamran.ali@xerobug.com>
2016-02-01 21:16:41 +0500

    camera fix

--- js/pages/sale-photo.js
+++ js/pages/sale-photo.js
@@ -2,7 +2,7 @@
 // this function will capture customer photo
 function capturePhotoWithData() {
 	// Take picture using device camera and retrieve image as base64-encoded string
-	navigator.camera.getPicture(onPhotoDataSuccess, onFail, {sourceType:1, quality: 25,targetWidth: 415,targetHeight: 515,allowEdit : false, });
+	navigator.camera.getPicture(onPhotoDataSuccess, onFail, {sourceType:1, quality: 25,targetWidth: 415,targetHeight: 515,allowEdit : false,  destinationType: Camera.DestinationType.DATA_URL});
 }
 
 // this function will capture customer photo
