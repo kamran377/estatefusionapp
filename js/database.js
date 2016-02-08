fc70d51cb9281268ae6ad881db0fda6f643f37c9
Kamran Ali <kamran.ali@xerobug.com>
2016-02-08 12:42:10 +0500

    version number

--- js/database.js
+++ js/database.js
@@ -8,7 +8,7 @@
  * This file contains the methods / utilities related to the offline storage of app
  *
  *************************************************/
-var versionNumber = 'V'+moment().format('DDMMYY')+'.C';
+var versionNumber = 'V'+moment().format('DDMMYY')+'.A';
 $(document).on('ready',function(){
 	FastClick.attach(document.body);
 	window.isphone = false;
