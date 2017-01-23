10b271468e54986ad61e4d09c1d7991643e39840
Kamran Ali <kamran_ali@hotmail.com>
2017-01-23 11:24:26 +0500

    version number

--- js/database.js
+++ js/database.js
@@ -8,7 +8,7 @@
  * This file contains the methods / utilities related to the offline storage of app
  *
  *************************************************/
-var versionNumber = 'V210117.A';
+var versionNumber = 'V230117.A';
 
 $(document).on('ready',function(){
 	FastClick.attach(document.body);
