b485ae975fad31ca331ace284a8f29998b482453
Kamran Ali <kamran_ali@hotmail.com>
2017-02-16 16:04:15 +0500

    text to numbers for numeric fields

--- js/database.js
+++ js/database.js
@@ -8,7 +8,7 @@
  * This file contains the methods / utilities related to the offline storage of app
  *
  *************************************************/
-var versionNumber = 'MAIN-V160217.C';
+var versionNumber = 'MAIN-V160217.D';
 
 $(document).on('ready',function(){
 	FastClick.attach(document.body);
